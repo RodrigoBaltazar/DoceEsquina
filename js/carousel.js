d88970fefe1261b1e369b330975f9fec1d84ff68
Rodrigo Baltazar <rlbaltazar2012@gmail.com>
2017-11-05 19:33:12 -0200

    smartSpeed correcao

--- js/carousel.js
+++ js/carousel.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       margin:10,
       autoplay:true,
       autoplayTimeout:1000,
-      smartSpeed: 2500,
+      smartSpeed: 18000,
       autoplayHoverPause:true,
       responsive:{
         0:{
