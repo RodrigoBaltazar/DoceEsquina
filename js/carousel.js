40c84858454fdf5d975c250d96d3fe17f7359a5e
Rodrigo Baltazar <rlbaltazar2012@gmail.com>
2017-11-05 19:27:07 -0200

    adicionando meta tags

--- js/carousel.js
+++ js/carousel.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
       margin:10,
       autoplay:true,
       autoplayTimeout:1000,
+      smartSpeed: 2500,
       autoplayHoverPause:true,
       responsive:{
         0:{
